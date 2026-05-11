84f07c099df9c4acae665eb3548007aafb068077
Jawad <jawadd.code@gmail.com>
2026-05-11 23:57:26 +0500

    remove: *.js files from root

--- fix-dash-emoji.js
+++
@@ -1,8 +0,0 @@
-﻿const fs = require('fs');
-const files = ['src/page-components/Dashboard.tsx', 'src/page-components/Earnings.tsx', 'src/page-components/Inventory.tsx', 'src/page-components/Stores.tsx'];
-
-for(const f of files) {
-  let c = fs.readFileSync(f, 'utf8');
-  c = c.replace(/icon="[^"]+"/g, ''); // Remove emojis from icons
-  fs.writeFileSync(f, c, 'utf8');
-}
