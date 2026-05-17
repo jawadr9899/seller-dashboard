68caa621dcd765052bf89cab8e9d0a666b82dc98
Jawad <jawadd.code@gmail.com>
2026-05-17 21:50:00 +0500

    update: remove location icon & shortened text from nearby tab

--- src/page-components/NearbyStores.tsx
+++ src/page-components/NearbyStores.tsx
@@ -138,20 +138,7 @@ export default function NearbyStores() {
                     : "text-[#7da2a9] hover:text-[#00a3b4]"
                 }`}
               >
-                {activeTab === "nearby" && (
-                  <svg
-                    className="w-4 h-4"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                )}
-                Nearby Products
+                Nearby
               </button>
               <button
                 onClick={() => {
